c0028d4b05770e1d7ee89c65389f84e0e01a314f
Vivek Nair <vivek@nair.me>
2022-01-11 12:01:34 -0800

    basic proxy

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const http = require('http')
-const httpProxy = require('http-proxy')
-const port = 8000
+const http = require('http');
+const httpProxy = require('http-proxy');
 
-httpProxy.createProxyServer({
-
-}).listen(port)
+const proxy = httpProxy.createProxyServer({});
+http.createServer(function(req, res) {
+  console.log('Request', req.method, req.url);
+  proxy.web(req, res, { target: `${req.protocol}://${req.hostname}` });
+}).listen(3000);
